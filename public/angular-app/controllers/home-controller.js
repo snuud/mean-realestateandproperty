f22a5f46b7873cfcf8cc714062f44d63f1362ef4
snuud <sn.snuud@gmail.com>
2018-02-09 18:37:02 +0800

    x

--- public/angular-app/controllers/home-controller.js
+++ public/angular-app/controllers/home-controller.js
@@ -20,18 +20,18 @@ function HomeController(dataFactory){
             interval: 2000
         });
 
-        function initMap(){
-            var uluru = {lat: -25.363, lng: 131.044};
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: uluru
-            });
+        // function initMap(){
+        //     var uluru = {lat: -25.363, lng: 131.044};
+        //     var map = new google.maps.Map(document.getElementById('map'), {
+        //         zoom: 4,
+        //         center: uluru
+        //     });
 
-            var marker = new google.maps.Marker({
-                position: uluru,
-                map: map
-            });
-        }
+        //     var marker = new google.maps.Marker({
+        //         position: uluru,
+        //         map: map
+        //     });
+        // }
     }).catch(function(error){
         console.log(error);
     });
